a9a168d8139a3caa98ce85ef3c56ac4c2a6bb182
dapskuycode <138641256+dapskuycode@users.noreply.github.com>
2025-03-09 12:20:47 +0700

    Update JumlahPesananMelebihiStokException.java

--- santapanEnak/JumlahPesananMelebihiStokException.java
+++ santapanEnak/JumlahPesananMelebihiStokException.java
@@ -1,3 +1,9 @@
+/* Nama File    : JumlahPesananMelebihiStokException.java
+ * Deskripsi    : menangani error saat jumlah pesanan yang dibuat melebihi stok yang ada.
+ * Pembuat      : M. Daffa' Atstsaqif/24060123120039
+ * Tanggal      : 8 Maret 2025
+ */
+
 package santapanEnak;
 
 public class JumlahPesananMelebihiStokException extends Exception{
