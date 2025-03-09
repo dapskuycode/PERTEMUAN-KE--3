b9c59607e9ef9875580f1bafe5008841d7b51d81
dapskuycode <138641256+dapskuycode@users.noreply.github.com>
2025-03-09 10:21:56 +0700

    Update AngkaSialException.java

--- Asersil/AngkaSialException.java
+++ Asersil/AngkaSialException.java
@@ -1,3 +1,9 @@
+/* Nama File    : AngkaSialException.java
+ * Deskripsi    : contoh pembuatan exception custom
+ * Pembuat      : M. Daffa' Atstsaqif/24060123120039
+ * Tanggal      : 8 Maret 2025
+ */
+
 package Asersil;
 
 public class AngkaSialException extends Exception {
