94936c2fa57eefd5e95c4ad1adfbcceb4987b6f2
dapskuycode <138641256+dapskuycode@users.noreply.github.com>
2025-03-09 12:24:59 +0700

    Update MenuTidakTersediaException.java

--- santapanEnak/MenuTidakTersediaException.java
+++ santapanEnak/MenuTidakTersediaException.java
@@ -1,3 +1,10 @@
+/* Nama File    : MenuTidakTersedia.Exception.java
+ * Deskripsi    : menangani error saat menu yang dipesan tidak tersedia.
+ * Pembuat      : M. Daffa' Atstsaqif/24060123120039
+ * Tanggal      : 8 Maret 2025
+ */
+
+
 package santapanEnak;
 
 public class MenuTidakTersediaException extends Exception{
